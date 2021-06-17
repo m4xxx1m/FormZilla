b2eac25215ad9bd8f60f5131e4c724758560eba4
KapustaKosta <k.starikov@lenta.ru>
2021-06-17 16:45:15 +0400

    Added class UserAnswer which is used only in the class Form

--- app/src/main/java/ru/raptors/team/formzilla/models/Form.java
+++ app/src/main/java/ru/raptors/team/formzilla/models/Form.java
@@ -1,16 +1,25 @@
 package ru.raptors.team.formzilla.models;
 
+import android.content.Context;
+
 import java.util.ArrayList;
 
 import ru.raptors.team.formzilla.enums.FormStatus;
+import ru.raptors.team.formzilla.interfaces.Action;
 
 public class Form {
     private String ID;
     private FormStatus status;
     public ArrayList<Question> questions;
     public ArrayList<User> staff;
+    public ArrayList<UserAnswer> userAnswers;
 
     public void sendToStaff()
+    {
+
+    }
+
+    public void getStaffAnswersAndDoAction(Context context, Action action)
     {
 
     }
