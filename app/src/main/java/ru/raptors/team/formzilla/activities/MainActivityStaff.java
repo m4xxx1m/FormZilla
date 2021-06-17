3f939ec00857364e68171661c0242cf692047e18
m4xxx1m <mi4469228@yandex.ru>
2021-06-17 17:01:37 +0400

    some changes

--- app/src/main/java/ru/raptors/team/formzilla/activities/MainActivityStaff.java
+++ app/src/main/java/ru/raptors/team/formzilla/activities/MainActivityStaff.java
@@ -1,13 +1,76 @@
 package ru.raptors.team.formzilla.activities;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import ru.raptors.team.formzilla.R;
 
 public class MainActivityStaff extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.activity_main_staff);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBar supportActionBar = getSupportActionBar();
+        VectorDrawableCompat indicator
+                = VectorDrawableCompat.create(getResources(), R.drawable.ic_menu, getTheme());
+        indicator.setTint(ResourcesCompat.getColor(getResources(), R.color.white, getTheme()));
+        supportActionBar.setHomeAsUpIndicator(indicator);
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        mDrawerLayout = findViewById(R.id.drawer);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ConstraintLayout placeHolder = findViewById(R.id.place_holder);
+        RecyclerView recyclerView = null;
+        if (true) {
+            placeHolder.removeView(findViewById(R.id.recycler_view));
+            TextView tv = (TextView) getLayoutInflater().inflate(R.layout.no_available_forms, placeHolder);
+            tv.setText("У вас пока нет доступных опросов");
+            placeHolder.addView(tv);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
