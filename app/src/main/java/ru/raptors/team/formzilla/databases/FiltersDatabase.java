c9814456fbeea0d956dc484e7199d0a408884c2a
KapustaKosta <k.starikov@lenta.ru>
2021-06-26 22:08:17 +0400

    Added SQLite databases and saving/loading from locale databases New classes: FiltersDatabase FormsDatabase QuestionsDatabase UsersDatabase

---
+++ app/src/main/java/ru/raptors/team/formzilla/databases/FiltersDatabase.java
@@ -0,0 +1,135 @@
+package ru.raptors.team.formzilla.databases;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import ru.raptors.team.formzilla.models.Filter;
+import ru.raptors.team.formzilla.models.Question;
+import ru.raptors.team.formzilla.models.QuestionType;
+
+public class FiltersDatabase implements Serializable {
+    private static final String DATABASE_NAME = "filters.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "SavedFilters";
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_FILTER = "filter";
+    private static final String COLUMN_CATEGORY = "category";
+    private static final String COLUMN_STAFF = "staff";
+
+    private static final int NUM_COLUMN_ID = 0;
+    private static final int NUM_COLUMN_FILTER = 1;
+    private static final int NUM_COLUMN_CATEGORY = 2;
+    private static final int NUM_COLUMN_STAFF = 3;
+
+    private SQLiteDatabase database;
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " STRING PRIMARY KEY," +
+                    COLUMN_FILTER + " TEXT," +
+                    COLUMN_CATEGORY + " TEXT," +
+                    COLUMN_STAFF + " TEXT)";
+
+    public FiltersDatabase (Context context)
+    {
+        FiltersDatabase.OpenHelper openHelper = new FiltersDatabase.OpenHelper(context);
+        database = openHelper.getWritableDatabase();
+    }
+
+    public long insert(Filter filter) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_ID, filter.ID);
+        contentValues.put(COLUMN_FILTER, filter.filter);
+        contentValues.put(COLUMN_CATEGORY, filter.category);
+        contentValues.put(COLUMN_STAFF, filter.packStaff());
+
+        return database.insert(TABLE_NAME, null, contentValues);
+    }
+
+    public int update(Filter filter) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_ID, filter.ID);
+        contentValues.put(COLUMN_FILTER, filter.filter);
+        contentValues.put(COLUMN_CATEGORY, filter.category);
+        contentValues.put(COLUMN_STAFF, filter.packStaff());
+
+        return database.update(TABLE_NAME, contentValues, COLUMN_ID + " = ?",new String[] { String.valueOf(filter.ID)});
+    }
+
+    public void deleteAll() {
+        database.delete(TABLE_NAME, null, null);
+    }
+
+    public void delete(String id) {
+        database.delete(TABLE_NAME, COLUMN_ID + " = ?", new String[] { String.valueOf(id) });
+    }
+
+    public Filter select(String id) {
+        Cursor cursor = database.query(TABLE_NAME, null, COLUMN_ID + " = ?", new String[]{String.valueOf(id)}, null, null, null);
+
+        cursor.moveToFirst();
+        if(cursor.getCount() == 0) return null;
+        String filter = cursor.getString(NUM_COLUMN_FILTER);
+        String category = cursor.getString(NUM_COLUMN_CATEGORY);
+        String staff = cursor.getString(NUM_COLUMN_STAFF);
+        Filter result = new Filter(id);
+        result.filter = filter;
+        result.category = category;
+        result.unpackStaff(staff);
+
+        return result;
+    }
+
+    public ArrayList<Filter> selectAll() {
+        Cursor cursor = database.query(TABLE_NAME, null, null, null, null, null, null);
+
+        ArrayList<Filter> arrayList = new ArrayList<Filter>();
+        cursor.moveToFirst();
+        if (!cursor.isAfterLast()) {
+            do {
+                String id = cursor.getString(NUM_COLUMN_ID);
+                String filter = cursor.getString(NUM_COLUMN_FILTER);
+                String category = cursor.getString(NUM_COLUMN_CATEGORY);
+                String staff = cursor.getString(NUM_COLUMN_STAFF);
+                Filter result = new Filter(id);
+                result.filter = filter;
+                result.category = category;
+                result.unpackStaff(staff);
+                arrayList.add(result);
+            } while (cursor.moveToNext());
+        }
+        return arrayList;
+    }
+
+    public boolean hasFilter(String id) {
+        boolean result = true;
+        Cursor cursor = database.query(TABLE_NAME, null, COLUMN_ID + " = ?", new String[]{String.valueOf(id)}, null, null, null);
+        if (cursor.getCount() == 0) result = false;
+        return result;
+    }
+
+    private class OpenHelper extends SQLiteOpenHelper implements Serializable {
+
+        OpenHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(db);
+        }
+    }
+}
--- app/src/main/java/ru/raptors/team/formzilla/models/User.java
+++ app/src/main/java/ru/raptors/team/formzilla/models/User.java
@@ -12,6 +12,8 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import ru.raptors.team.formzilla.databases.FormsDatabase;
+import ru.raptors.team.formzilla.databases.UsersDatabase;
 import ru.raptors.team.formzilla.enums.FormStatusEnum;
 import ru.raptors.team.formzilla.enums.GenderEnum;
 import ru.raptors.team.formzilla.interfaces.Action;
@@ -147,7 +149,9 @@ public class User implements Saveable {
 
     public void save(Context context)
     {
-        // Todo: сохраняет данные пользователя на смартфоне в SQLite
+        UsersDatabase usersDatabase;
+        usersDatabase = new UsersDatabase(context);
+        usersDatabase.update(this);
     }
 
     public void saveInFirebase(Context context)
@@ -173,7 +177,17 @@ public class User implements Saveable {
 
     public void loadFromPhone(Context context)
     {
-        // Todo: загружает информацию о пользователе с SQLite
+        UsersDatabase usersDatabase;
+        usersDatabase = new UsersDatabase(context);
+        User user = usersDatabase.select(ID);
+        this.gender = user.gender;
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.company = user.getCompany();
+        this.companyID = user.getCompanyID();
+        this.forms = user.forms;
+        this.staff = user.staff;
+        this.filters = user.filters;
     }
 
     private Form findFormByID(String ID)
@@ -230,6 +244,67 @@ public class User implements Saveable {
         }
     }
 
+    public String packStaff()
+    {
+        String result = "";
+        for(User user : staff)
+        {
+            result += user.getID() + " ";
+        }
+        return result;
+    }
+
+    public void unpackStaff(String pack)
+    {
+        String[] unpackedStaffID = pack.split(" ");
+        for(String unpackedUserID : unpackedStaffID)
+        {
+            staff.add(new User(unpackedUserID));
+        }
+    }
+
+    public String packForms()
+    {
+        String result = "";
+        for(Form form : forms)
+        {
+            result += form.getID() + " ";
+        }
+        return result;
+    }
+
+    public void unpackForms(String pack, Context context)
+    {
+        String[] unpackedFormsIDs = pack.split(" ");
+        for(String unpackedFormID : unpackedFormsIDs)
+        {
+            Form form = new Form(unpackedFormID);
+            form.loadFromPhone(context);
+            forms.add(form);
+        }
+    }
+
+    public String packFilters()
+    {
+        String result = "";
+        for(Filter filter : filters)
+        {
+            result += filter.ID + " ";
+        }
+        return result;
+    }
+
+    public void unpackFilters(String pack, Context context)
+    {
+        String[] unpackedFiltersIDs = pack.split(" ");
+        for(String unpackedFilterID : unpackedFiltersIDs)
+        {
+            Filter filter = new Filter(unpackedFilterID);
+            filter.loadFromPhone(context);
+            filters.add(filter);
+        }
+    }
+
     public ArrayList<Form> getAvailableForms() {
         ArrayList<Form> result = new ArrayList<Form>();
         for(Form form : forms)
@@ -263,5 +338,45 @@ public class User implements Saveable {
 
     public ArrayList<Form> getForms() {
         return forms;
+    }
+
+    public GenderEnum getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderEnum gender) {
+        this.gender = gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCompanyID() {
+        return companyID;
+    }
+
+    public void setCompanyID(String companyID) {
+        this.companyID = companyID;
     }
 }
