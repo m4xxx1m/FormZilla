4c76f5f3741835eafab0d4b1c51e3b6e159da63d
m4xxx1m <mi4469228@yandex.ru>
2021-07-01 16:57:46 +0400

    created new employee activity

---
+++ app/src/main/java/ru/raptors/team/formzilla/activities/NewEmployeeActivity.java
@@ -0,0 +1,16 @@
+package ru.raptors.team.formzilla.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.raptors.team.formzilla.R;
+
+public class NewEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_employee);
+    }
+}
