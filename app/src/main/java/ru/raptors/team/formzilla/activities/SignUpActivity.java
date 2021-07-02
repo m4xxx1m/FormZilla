035ac9fb101198c79b0ef7686733dc533aed8d4f
m4xxx1m <mi4469228@yandex.ru>
2021-07-02 17:47:21 +0400

    added sign up activity

---
+++ app/src/main/java/ru/raptors/team/formzilla/activities/SignUpActivity.java
@@ -0,0 +1,13 @@
+package ru.raptors.team.formzilla.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
