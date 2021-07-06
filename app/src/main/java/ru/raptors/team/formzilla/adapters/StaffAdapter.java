4b98fa7786bdf2085e2fddb06f02b8932b5992c8
ny-takoe <79892036+ny-takoe@users.noreply.github.com>
2021-07-06 15:44:09 +0400

    now you can copy information about your staff

--- app/src/main/java/ru/raptors/team/formzilla/adapters/StaffAdapter.java
+++ app/src/main/java/ru/raptors/team/formzilla/adapters/StaffAdapter.java
@@ -1,12 +1,18 @@
 package ru.raptors.team.formzilla.adapters;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
 
@@ -17,10 +23,12 @@ import ru.raptors.team.formzilla.models.User;
 public class StaffAdapter extends RecyclerView.Adapter<StaffAdapter.ViewHolder>{
     private LayoutInflater inflater;
     private List<User> staff;
+    private Context context;
 
     public StaffAdapter(Context context, List<User> staff) {
         this.staff = staff;
         this.inflater = LayoutInflater.from(context);
+        this.context = context;
     }
     @Override
     public StaffAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -35,6 +43,22 @@ public class StaffAdapter extends RecyclerView.Adapter<StaffAdapter.ViewHolder>{
         holder.firstLastName.setText(user.getFirstName() + " " + user.getLastName());
         holder.login.setText(user.getLogin());
         holder.password.setText(user.getPassword());
+        holder.placeHolder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+                    String infoToCopy = holder.firstLastName.getText() + "\nЛогин: " + holder.login.getText() +
+                            "\nПароль: " + holder.password.getText();
+                    ClipData clip = ClipData.newPlainText("copy information about employee", infoToCopy);
+                    clipboard.setPrimaryClip(clip);
+                    Snackbar.make((View) holder.placeHolder, "Скопировано", Snackbar.LENGTH_SHORT).show();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
@@ -46,11 +70,13 @@ public class StaffAdapter extends RecyclerView.Adapter<StaffAdapter.ViewHolder>{
         final TextView firstLastName;
         final TextView login;
         final TextView password;
+        final RelativeLayout placeHolder;
         ViewHolder(View view){
             super(view);
             firstLastName = view.findViewById(R.id.firstname_lastname_text_view);
             login = view.findViewById(R.id.login_text_view);
             password = view.findViewById(R.id.password_text_view);
+            placeHolder = view.findViewById(R.id.employee);
         }
     }
 }
